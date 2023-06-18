3124fb8fd67d17c97c8b6171689f343cb9b7a9bc
iMeaNz <mehdy.morvan@epitech.eu>
2023-06-18 15:39:22 +0200

    FIX: Communication now works, but still need to remove the JSON decoding because we aren't receiving json

--- ai/server.py
+++ ai/server.py
@@ -14,13 +14,14 @@ class ZappyClient:
             print("Machine {} connected on port {}".format(self.machine, self.port))
             self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.client_socket.connect((self.machine, self.port))
-            self.send(self.name)
+            self.receive()
+            self.send(self.name + "\n")
         except Exception as e:
             print(f"Error connecting to the server: {e}")
 
     def send(self, message):
         try:
-            self.client_socket.sendall(message.encode('utf-8'))
+            self.client_socket.sendall(message.encode('ascii'))
         except Exception as e:
             print(f"Error sending message: {e}")
 
@@ -30,7 +31,7 @@ class ZappyClient:
             while True:
                 part = self.client_socket.recv(1024)
                 if part:
-                    data += part.decode('utf-8')
+                    data += part.decode('ascii')
                     if data.endswith('\n'):
                         break
                 else:
@@ -46,27 +47,27 @@ class ZappyClient:
             print(f"Error closing the socket: {e}")
 
     def move_forward(self):
-        self.send('Forward')
+        self.send('Forward\n')
         return self.receive()
 
     def turn_right(self):
-        self.send('Right')
+        self.send('Right\n')
         return self.receive()
 
     def turn_left(self):
-        self.send('Left')
+        self.send('Left\n')
         return self.receive()
 
     def look_around(self):
-        self.send('Look')
+        self.send('Look\n')
         response = self.receive()
         print(f'Response: {response}')
         if response:
             try:
                 tiles = json.loads(response)
                 return tiles
-            except json.JSONDecodeError:
-                print("Error decoding JSON response")
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON response : {str(e)}")
                 return None
         else:
             return None
