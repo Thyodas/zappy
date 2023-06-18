c81d99c65defaea370dc5e3a3914d2d9a25b4468
Nathan <nathan.de-balthasar-de-gacheo@epitech.eu>
2023-06-18 14:32:30 +0200

    feat: class to get program parameters

--- gui/src/Main.cpp
+++ gui/src/Main.cpp
@@ -9,10 +9,17 @@
 #include "Core.hpp"
 #include <memory>
 #include <iostream>
+#include "Message.h"
+#include "ArgsParser.hpp"
 
 int main([[maybe_unused]] int argc, char **argv)
 {
+    if (argc == 2 && std::string(argv[1]).compare("-help") == 0) {
+        std::cout << HELP_MESSAGE << std::endl;
+        return 0;
+    }
     try {
+        GUI::Args args = GUI::ArgsParser::parseArgs(argc, argv);
         std::unique_ptr<GUI::ICore> core = std::make_unique<GUI::Core>();
         core->init(GUI::GraphicalLib::RAYLIB, {800, 600});
         core->run();
